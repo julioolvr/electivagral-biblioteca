ecafa2fd111eace0f1fa3b8f2b7551a0cf252a97
Julio Olivera <julio.olvr@gmail.com>
2012-09-15 09:05:09 -0300

    Cambiado el comportamiento de la BD en dev

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -14,7 +14,7 @@ environments {
     development {
         dataSource {
 			driverClassName = "com.mysql.jdbc.Driver"
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
             url = "jdbc:mysql://localhost/biblioteca"
 			username = "root"
 			password = "root"
